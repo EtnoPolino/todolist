2e10d74520b09e4dea57d6192ec137ea5f088bfe
EtnoPolino <einshoch66@gmail.com>
2022-04-07 04:12:07 +0200

    bouton submit qui fonctionne et liste les entrés

--- script.js
+++ script.js
@@ -2,9 +2,25 @@ const submit = document.querySelector('button');
 const listContainer = document.querySelector('.toDoContainer');
 const input = document.querySelector('input');
 
-function submitInput(){
-   
-  
+function submitInput(event){
 
+    if(!input.value) return;
+
+    const paragraph = document.createElement('p');
+    const sautLigne = document.createElement('br');
+    paragraph.style.display = "inline-block";
+    listContainer.appendChild(paragraph);
+    listContainer.appendChild(sautLigne);
+    paragraph.innerHTML = input.value;
+    
+    input.value='';
 }
+
+function endTask(e){
+        
+}
+
+
 submit.addEventListener('click', submitInput);
+
+listContainer.addEventListener('click', endTask)
